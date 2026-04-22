9169b14885d3a4318aa508c04f32cc001b820bb7
TuanDung <dungtuanxz12345@gmail.com>
2026-04-22 17:29:01 +0700

    Server Reply Client

--- RemoteCommandServer/Program.cs
+++ RemoteCommandServer/Program.cs
@@ -13,6 +13,16 @@ class Server
 
         TcpClient client = listener.AcceptTcpClient();
         Console.WriteLine("Client connected!");
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+        string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received: " + message);
+
+        string response = "Server received: " + message;
+        byte[] data = Encoding.UTF8.GetBytes(response);
+
+        stream.Write(data, 0, data.Length);
 
         Console.ReadLine();
     }
