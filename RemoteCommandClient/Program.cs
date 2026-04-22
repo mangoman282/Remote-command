706074e0d99527f45b3ada2eac54fd46d100d9ad
Tuaan06 <tuanp7430@ut.edu.vn>
2026-04-22 17:52:45 +0700

    Client sending

--- RemoteCommandClient/Program.cs
+++ RemoteCommandClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Text;
 
 class Client
 {
@@ -7,6 +8,15 @@ class Client
     {
         TcpClient client = new TcpClient();
         client.Connect("127.0.0.1", 8080);
+
+                NetworkStream stream = client.GetStream();
+                        string msg = "Hello Server!";
+        byte[] data = Encoding.UTF8.GetBytes(msg);
+        stream.Write(data, 0, data.Length);
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
 
         Console.WriteLine("Connected to server!");
 
