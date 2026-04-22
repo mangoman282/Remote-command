cb8b1067ce5c097fab27d3d8586141458dfcd318
TuanDung <dungtuanxz12345@gmail.com>
2026-04-22 16:28:45 +0700

    Tao Server ket noi Client

---
+++ RemoteCommandServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+class Server
+{
+    static void Main()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+
+        Console.WriteLine("Server is listening on port 8080...");
+
+        TcpClient client = listener.AcceptTcpClient();
+        Console.WriteLine("Client connected!");
+
+        Console.ReadLine();
+    }
+}
