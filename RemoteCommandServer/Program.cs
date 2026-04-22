2ad2d97111a0f934138b7a8dc74a89bb9a13ae5f
Tuaan06 <tuanp7430@ut.edu.vn>
2026-04-22 18:13:52 +0700

    Update Client send real command to server

--- RemoteCommandServer/Program.cs
+++ RemoteCommandServer/Program.cs
@@ -1,31 +1,23 @@
-
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
-
+using System.Text;
 class Server
 {
     static void Main()
     {
         TcpListener listener = new TcpListener(IPAddress.Any, 8080);
         listener.Start();
-
-        Console.WriteLine("Server is listening on port 8080...");
-
+        Console.WriteLine("Server is listening...");
         TcpClient client = listener.AcceptTcpClient();
-        Console.WriteLine("Client connected!");
+        NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[1024];
         int bytesRead = stream.Read(buffer, 0, buffer.Length);
-
         string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
         Console.WriteLine("Received: " + message);
-
         string response = "Server received: " + message;
         byte[] data = Encoding.UTF8.GetBytes(response);
-
         stream.Write(data, 0, data.Length);
-
         Console.ReadLine();
     }
-
 }
