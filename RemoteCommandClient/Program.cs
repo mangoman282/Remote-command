ee27eb1c2e21dece6aa2a3ad4a39dbcff0c7ae2d
Tuaan06 <tuanp7430@ut.edu.vn>
2026-04-22 16:47:17 +0700

    Add RemoteCommandClient

---
+++ RemoteCommandClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Sockets;
+
+class Client
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient();
+        client.Connect("127.0.0.1", 8080);
+
+        Console.WriteLine("Connected to server!");
+
+        Console.ReadLine();
+    }
+}
